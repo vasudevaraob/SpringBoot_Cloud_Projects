f46ecc4b362c60b0521f57a9b1747d3d8b840d6f
vasudevaraob <29686823+vasudevaraob@users.noreply.github.com>
2021-03-25 00:43:30 +0530

    Initial security got updated

---
+++ spring-security-inmemory-authentication-example/src/main/java/com/springboot/security/SpringSecurityInmemoryAuthenticationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInmemoryAuthenticationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInmemoryAuthenticationExampleApplication.class, args);
+	}
+
+}
